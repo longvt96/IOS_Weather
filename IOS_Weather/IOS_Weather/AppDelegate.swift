8e0bd317084fdf1b04db7d7f8a41c7d25f85b1b9
longvu <thanhlong140496@gmail.com>
2018-04-05 10:04:57 +0700

    task1_init_project

---
+++ IOS_Weather/IOS_Weather/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  IOS_Weather
+//
+//  Created by ThanhLong on 4/5/18.
+//  Copyright © 2018 ThanhLong. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+    }
+}
