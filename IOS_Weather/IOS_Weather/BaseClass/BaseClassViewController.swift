1802585b452fe22263b242556e63cb947d9addf6
longvu <thanhlong140496@gmail.com>
2018-04-06 13:22:24 +0700

    task_3_init_main_screen

---
+++ IOS_Weather/IOS_Weather/BaseClass/BaseClassViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BaseClassViewController.swift
+//  IOS_Weather
+//
+//  Created by ThanhLong on 4/6/18.
+//  Copyright © 2018 ThanhLong. All rights reserved.
+//
+
+import UIKit
+import NVActivityIndicatorView
+
+class BaseClassViewController: UIViewController, NVActivityIndicatorViewable {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func showLoadingOnParent() {
+        let size = CGSize(width: 30, height: 30)
+        startAnimating(size, message: "", messageFont: nil,
+                       type: NVActivityIndicatorType(rawValue: 16)!,
+                       color: UIColor.red, padding: 0, displayTimeThreshold: 0,
+                       minimumDisplayTime: 0,
+                       backgroundColor: .clear, textColor: UIColor.red)
+    }
+
+    func hidenLoading() {
+        stopAnimating()
+    }
+}
--- IOS_Weather/IOS_Weather/MainViewController.swift
+++ IOS_Weather/IOS_Weather/MainViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: BaseClassViewController {
 
     private let placeRepository: PlaceRepositories = PlaceRepositoriesImp(api: APIService.share)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.showLoadingOnParent()
         placeRepository.getweather(lat: 37.8267, lot: -122.4233) { (result) in
             switch result {
             case .success(let weatherResponse):
@@ -21,8 +22,10 @@ class ViewController: UIViewController {
                    let place = weatherdemo.place
                     print("place: \(place!.time!) \(String(describing: place!.summary!))")
                 }
+                self.hidenLoading()
             case .failure(let error):
                 print("\(String(describing: error))")
+                self.hidenLoading()
             }
         }
     }
